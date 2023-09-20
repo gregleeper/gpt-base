b2767a9de7e969acfce6ec1d79e90a629da6845a
{{Greg Leeper}} <{{gregleeper19@gmail.com}}>
2023-09-20 18:16:36 -0500

    moved to planetscale

--- prisma/migrations/20230920104845_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Context" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "rubric" TEXT NOT NULL,
-    "requirements" TEXT NOT NULL,
-    "assistantRole" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
